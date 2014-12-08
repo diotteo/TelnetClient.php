e9fffb55f03a611443bac8835bb3639cf22ddca3
Olivier Diotte <olivier+github@diotte.ca>
2014-12-07 21:17:08 -0500

    TelnetClient.php: Cosmetic fix

--- TelnetClient.php
+++ TelnetClient.php
@@ -805,8 +805,9 @@ class TelnetClient {
 		 * since we return as soon as the prompt is found (regardless of whether or not more characters have been received or not), appending to the global buffer means those characters get written after (while they were received before)
 		 */
 		//$this->global_buffer .= $buffer;
+
 		$ret = fwrite($this->socket, $buffer);
-		if ($ret !== strlen($buffer)) { //|| $ret === false) {
+		if ($ret !== strlen($buffer)) { //|| $ret === false)
 			throw new ConnectionException("Error writing to socket");
 		}
 
